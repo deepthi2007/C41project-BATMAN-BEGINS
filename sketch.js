e3243d6398d5452e9b7d8661d2aeb6fd62f3a8e3
sai deepthi <62826707+deepthi2007@users.noreply.github.com>
2020-11-15 18:56:07 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -9,18 +9,18 @@ var drops=[];
 
 
 function preload(){
- umbrellaImg = loadAnimation("images/Walking Frame/walking_1.png",
- "images/Walking Frame/walking_2.png","images/Walking Frame/walking_3.png",
- "images/Walking Frame/walking_4.png","images/Walking Frame/walking_5.png",
- "images/Walking Frame/walking_6.png", "images/Walking Frame/walking_7.png",
- "images/Walking Frame/walking_8.png");
+ umbrellaImg = loadAnimation("walking_1.png",
+ "walking_2.png","walking_3.png",
+ "walking_4.png","walking_5.png",
+ "walking_6.png", "walking_7.png",
+ "walking_8.png");
 
- ThunderImg1=loadImage("images/thunderbolt/1.png");
- ThunderImg2=loadImage("images/thunderbolt/2.png");
- ThunderImg3=loadImage("images/thunderbolt/3.png");
- ThunderImg4=loadImage("images/thunderbolt/4.png");
+ ThunderImg1=loadImage("1.png");
+ ThunderImg2=loadImage("2.png");
+ ThunderImg3=loadImage("3.png");
+ ThunderImg4=loadImage("4.png");
 
- rain = loadImage("images/rain.png")
+ rain = loadImage("rain.png")
 
 }
 
